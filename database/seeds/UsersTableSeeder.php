cbdd60f0034cae4a71e2e006f40ba460741c38f1
Rahmat Awaludin <rahmat.awaludin@gmail.com>
2018-02-17 13:10:52 +0700

    added api_token to users table

--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -17,6 +17,7 @@ class UsersTableSeeder extends Seeder
             'email' => 'admin@gmail.com',
             'password' => bcrypt('rahasia'),
             'level' => User::ACCESS_ADMIN,
+            'api_token' => '3ad12acc-80d4-3f50-93fa-e70d30947f19'
         ]);
 
         factory(User::class)->create([
@@ -24,6 +25,7 @@ class UsersTableSeeder extends Seeder
             'email' => 'budi@gmail.com',
             'password' => bcrypt('rahasia'),
             'level' => User::ACCESS_MEMBER,
+            'api_token' => 'f640f7d0-f7b3-3991-96d1-5df1b5f3fe6d'
         ]);
 
         factory(User::class)->create([
@@ -31,6 +33,7 @@ class UsersTableSeeder extends Seeder
             'email' => 'dadang@gmail.com',
             'password' => bcrypt('rahasia'),
             'level' => User::ACCESS_MEMBER,
+            'api_token' => '820b0341-7a01-3a40-ac3c-9160b69d8c8b'
         ]);
     }
 }
