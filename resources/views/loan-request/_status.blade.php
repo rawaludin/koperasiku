fa641990912e0abe97f38d77f423d20c69735c9a
Rahmat Awaludin <rahmat.awaludin@gmail.com>
2018-02-17 11:23:24 +0700

    dynamic loan request status link

--- resources/views/loan-request/_status.blade.php
+++ resources/views/loan-request/_status.blade.php
@@ -1,7 +1,15 @@
-{!! Form::open(['route' => ['loan-requests.destroy', $loanRequest->id], 'method' => 'delete', 'class' => 'form-inline'] ) !!}
-    <a href="{{ route('loan-requests.show', $loanRequest->id) }}">Tampilkan </a> | 
-    <a href="{{ route('loan-requests.edit', $loanRequest->id) }}">Ubah</a>
-    {!! Form::hidden('page', request()->get('page')) !!}
-    {!! Form::submit('Batalkan', ['class'=>'btn btn-sm btn-danger']) !!}
-
-{!! Form::close()!!}
+@switch($loanRequest->status)
+    @case('Approved')
+    @case('Rejected')
+    @case('Waiting Approval')
+        <a href="{{ route('loan-requests.show', $loanRequest->id) }}">Tampilkan </a>
+        @break
+    @case('Draft')
+        {!! Form::open(['route' => ['loan-requests.destroy', $loanRequest->id], 'method' => 'delete', 'class' => 'form-inline'] ) !!}
+            <a href="{{ route('loan-requests.show', $loanRequest->id) }}">Tampilkan </a> | 
+            <a href="{{ route('loan-requests.edit', $loanRequest->id) }}">Ubah</a>
+            {!! Form::hidden('page', request()->get('page')) !!}
+            {!! Form::submit('Batalkan', ['class'=>'btn btn-sm btn-danger']) !!}
+        {!! Form::close()!!}
+        @break
+@endswitch
