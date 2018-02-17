d37af91e916610d41f1caacde1d7663327b2fcab
Rahmat Awaludin <rahmat.awaludin@gmail.com>
2018-02-17 10:35:13 +0700

    member middleware

--- routes/web.php
+++ routes/web.php
@@ -18,4 +18,6 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
-Route::resource('loan-requests', 'LoanRequestController');
+Route::middleware(['auth', 'member'])->group(function() {
+    Route::resource('loan-requests', 'LoanRequestController');
+});
